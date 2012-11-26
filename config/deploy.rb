6ae642995f78ef1bc70e4d7203675f09d15a0da2
Steve Kenworthy <steveyken@gmail.com>
2012-11-26 10:35:38 +0800

    Update passenger version

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :domain,             "crossroadsint.org"
 set :repository,         "git://github.com/crossroads/ffcrm_app.git"
 set :rvm_ruby_string,    "ruby-1.9.3-p125"
 set :rvm_type,           :root
-set :passenger_version,  "3.0.11"
+set :passenger_version,  "3.0.18"
 set :yum_packages,       %w(ImageMagick-devel libxml2 libxml2-devel libxslt libxslt-devel)
 set :default_stage,      "staging"
 set :normalize_asset_timestamps, false
