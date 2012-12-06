dd8715c8366de9b0439fe19b7af02c533c9b6a8d
Steve Kenworthy <steveyken@gmail.com>
2012-12-06 15:55:15 +0800

    Turn on asset debugging so we can see stylesheets and javascripts individually.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,5 @@ FatFreeCRM::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 end
